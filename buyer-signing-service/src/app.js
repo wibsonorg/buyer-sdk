dbf7ee74a42e9c1f2fdbd121a73583c03bfede15
Nico <ncls.ayala@gmail.com>
2018-07-03 19:05:56 -0300

    Add SS documentation

--- buyer-signing-service/src/app.js
+++ buyer-signing-service/src/app.js
@@ -3,16 +3,34 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
 import config from '../config';
 import { health } from './routes';
+
+const ls = dir =>
+  fs.readdirSync(dir)
+    .reduce((accumulator, file) => [...accumulator, `src/routes/${file}`], []);
 
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(morgan(config.env === 'development' ? 'dev' : 'combined'));
+if (config.env !== 'test') {
+  app.use(morgan(config.env === 'development' ? 'dev' : 'combined'));
+}
 app.use(cors());
 
 app.use('/health', health);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc({
+  swaggerDefinition: {
+    info: {
+      title: 'Buyer Signing Service',
+      version: '1.0.0',
+    },
+  },
+  apis: ls('src/routes'),
+})));
 
 export default app;
