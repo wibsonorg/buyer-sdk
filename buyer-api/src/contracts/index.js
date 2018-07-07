73666f01d1cd4c4ec33dcd8f0a4074b888ff4d6f
Daniel Fernandez <daniel@grandata.com>
2018-07-06 22:01:38 -0300

    getContracts method documented

--- buyer-api/src/contracts/index.js
+++ buyer-api/src/contracts/index.js
@@ -8,6 +8,16 @@ let dataTokenContractInstance = null;
 let dataExchangeContractInstance = null;
 let dataOrderContract = null;
 
+/**
+ * @async
+ * @function getContracts
+ * @param {Object} web3 the web3 object
+ * @param {String} dataTokenAddress the ethereum address to the data token used.
+ * @param {String} dataExchangeAddress the ethereum address to the data exchange used.
+ * @throws When truffle can not instantiate the contracts at the given addresses.
+ * @returns {Promise} Promise which resolves to an instance of the data token and
+ * the data exchange, and the configured data order truffle contract.
+ */
 const getContracts = async (web3, dataTokenAddress, dataExchangeAddress) => {
   if (dataTokenContractInstance && dataExchangeContractInstance && dataOrderContract) {
     return {
@@ -25,8 +35,8 @@ const getContracts = async (web3, dataTokenAddress, dataExchangeAddress) => {
   dataExchangeContract.setProvider(web3.currentProvider);
   dataOrderContract.setProvider(web3.currentProvider);
 
-  // TODO: Dirty hack to support web3@1.0.0 in truffle. Take it out when there is
-  // official support. See https://github.com/trufflesuite/truffle-contract/issues/56
+  // @TODO: Dirty hack to support web3@1.0.0 in truffle. Take it out when there is
+  // official support. @see {@link https://github.com/trufflesuite/truffle-contract/issues/56}
   dataTokenContract.currentProvider.sendAsync = () =>
     dataTokenContract.currentProvider.send(...arguments);
 
