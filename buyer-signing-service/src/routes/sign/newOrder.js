6d1299cd11e3412a80657683e57331135f5796c2
Nico <ncls.ayala@gmail.com>
2018-07-12 15:37:32 -0300

    Test endpoint

--- buyer-signing-service/src/routes/sign/newOrder.js
+++ buyer-signing-service/src/routes/sign/newOrder.js
@@ -4,31 +4,121 @@ import asyncError from '../../helpers';
 const router = express.Router();
 
 /**
+ * @TODO The schema is already defined in the swagger definition. Validation
+ *       shoud be done using that definition.
+ *
+ * Checks that every field is present.
+ *
+ * @return {Array} Error messages
+ */
+const validate = ({
+  nonce, // -> web3
+  gasPrice, // -> XXX
+  filters, // -> request
+  dataRequest, // -> request
+  price, // -> request
+  initialBudgetForAudits, // -> request
+  termsAndConditions, // -> Config
+  buyerURL, // -> Config
+}) => {
+  const fields = {
+    nonce: nonce && parseInt(nonce, 10),
+    gasPrice: gasPrice && parseInt(gasPrice, 10),
+    filters,
+    dataRequest,
+    price: price && parseInt(price, 10),
+    initialBudgetForAudits: initialBudgetForAudits &&
+      parseInt(initialBudgetForAudits, 10),
+    termsAndConditions,
+    buyerURL,
+  };
+
+  return Object.entries(fields).reduce((accumulator, [field, value]) => {
+    if (value === null || value === undefined) {
+      return [...accumulator, `Field '${field}' is mandatory`];
+    }
+
+    return accumulator;
+  }, []);
+};
+
+
+// * @param filters Target audience of the order.
+// * @param dataRequest Requested data type (Geolocation, Facebook, etc).
+// * @param price Price per added Data Response.
+// * @param initialBudgetForAudits The initial budget set for future audits.
+// * @param termsAndConditions Buyer's terms and conditions for the order.
+// * @param buyerURL Public URL of the buyer where the data must be sent.
+
+/**
  * @swagger
  * /sign/new-order:
  *   post:
  *     description: Signs a DataExchange.newOrder transaction
  *     produces:
  *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: nonce
+ *         type: integer
+ *         description: |
+ *           The number of transactions made by the sender prior to this one.
+ *         required: true
+ *       - in: body
+ *         name: gasPrice
+ *         type: integer
+ *         description: Gas price determined by the sender in wei (IS THIS OK?).
+ *         required: true
+ *       - in: body
+ *         name: transactionParameters
+ *         description: Parameters to be used in the transaction call.
+ *         required: true
+ *         schema:
+ *           $ref: "#/definitions/NewOrderParameters"
  *     responses:
  *       200:
  *         description: When the signing performs successfully
  *       500:
  *         description: Any other case
+ *
+ * definitions:
+ *   NewOrderParameters:
+ *     type: object
+ *     properties:
+ *       filters:
+ *         type: object
+ *         description: Target audience of the order.
+ *         required: true
+ *       dataRequest:
+ *         type: string
+ *         description: Requested data type (Geolocation, Facebook, etc).
+ *         required: true
+ *       price:
+ *         type: integer
+ *         description: Price per Data Response added.
+ *         required: true
+ *       initialBudgetForAudits:
+ *         type: integer
+ *         description: The initial budget set for future audits.
+ *         required: true
+ *       termsAndConditions:
+ *         type: string
+ *         description: Buyer's terms and conditions for the order.
+ *         required: true
+ *       buyerURL:
+ *         type: string
+ *         description: Public URL of the buyer where the data must be sent.
+ *         required: true
  */
 router.post('/new-order', asyncError(async (req, res) => {
-  const {
-    nonce, // -> web3
-    gasPrice, // -> XXX
-    filters, // -> request
-    dataRequest, // -> request
-    price, // -> request
-    initialBudgetForAudits, // -> request
-    termsAndConditions, // -> Config
-    buyerURL, // -> Config
-  } = req.body;
+  const { nonce, gasPrice, transactionParameters } = req.body;
+  const errors = validate({ nonce, gasPrice, ...transactionParameters });
 
-  res.json({ status: 'OK' });
+  if (errors.length > 0) {
+    res.boom.badData('Validation failed', { validation: errors });
+  } else {
+    res.json({ status: 'OK' });
+  }
 }));
 
 export default router;
--- buyer-signing-service/test/routes/sign/newOrderSpec.js
+++ buyer-signing-service/test/routes/sign/newOrderSpec.js
@@ -2,10 +2,225 @@ import request from 'supertest';
 import app from '../../../src/app';
 
 describe.only('/sign', () => {
+  const nonce = 0;
+  const gasPrice = 10000;
+  const filters = {
+    age: '30..35',
+  };
+  const dataRequest = 'geolocalization';
+  const price = 20;
+  const initialBudgetForAudits = 10;
+  const termsAndConditions = 'T&C';
+  const buyerURL = 'https://buyer.com';
+
   describe('POST /new-order', () => {
+    it('responds with an Unprocessable Entity status when nonce is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          gasPrice,
+          transactionParameters: {
+            filters,
+            dataRequest,
+            price,
+            initialBudgetForAudits,
+            termsAndConditions,
+            buyerURL,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'nonce' is mandatory",
+          ],
+        }, done);
+    });
+
+    it('responds with an Unprocessable Entity status when gasPrice is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          nonce,
+          transactionParameters: {
+            filters,
+            dataRequest,
+            price,
+            initialBudgetForAudits,
+            termsAndConditions,
+            buyerURL,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'gasPrice' is mandatory",
+          ],
+        }, done);
+    });
+
+    it('responds with an Unprocessable Entity status when filters is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          nonce,
+          gasPrice,
+          transactionParameters: {
+            dataRequest,
+            price,
+            initialBudgetForAudits,
+            termsAndConditions,
+            buyerURL,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'filters' is mandatory",
+          ],
+        }, done);
+    });
+
+    it('responds with an Unprocessable Entity status when dataRequest is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          nonce,
+          gasPrice,
+          transactionParameters: {
+            filters,
+            price,
+            initialBudgetForAudits,
+            termsAndConditions,
+            buyerURL,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'dataRequest' is mandatory",
+          ],
+        }, done);
+    });
+
+    it('responds with an Unprocessable Entity status when price is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          nonce,
+          gasPrice,
+          transactionParameters: {
+            filters,
+            dataRequest,
+            initialBudgetForAudits,
+            termsAndConditions,
+            buyerURL,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'price' is mandatory",
+          ],
+        }, done);
+    });
+
+    it('responds with an Unprocessable Entity status when initialBudgetForAudits is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          nonce,
+          gasPrice,
+          transactionParameters: {
+            filters,
+            dataRequest,
+            price,
+            termsAndConditions,
+            buyerURL,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'initialBudgetForAudits' is mandatory",
+          ],
+        }, done);
+    });
+
+    it('responds with an Unprocessable Entity status when termsAndConditions is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          nonce,
+          gasPrice,
+          transactionParameters: {
+            filters,
+            dataRequest,
+            price,
+            initialBudgetForAudits,
+            buyerURL,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'termsAndConditions' is mandatory",
+          ],
+        }, done);
+    });
+
+    it('responds with an Unprocessable Entity status when buyerURL is not present', (done) => {
+      request(app)
+        .post('/sign/new-order')
+        .send({
+          nonce,
+          gasPrice,
+          transactionParameters: {
+            filters,
+            dataRequest,
+            price,
+            initialBudgetForAudits,
+            termsAndConditions,
+          },
+        })
+        .expect(422, {
+          statusCode: 422,
+          error: 'Unprocessable Entity',
+          message: 'Validation failed',
+          validation: [
+            "Field 'buyerURL' is mandatory",
+          ],
+        }, done);
+    });
+
     it('responds with an OK status', (done) => {
       request(app)
         .post('/sign/new-order')
+        .send({
+          nonce,
+          gasPrice,
+          transactionParameters: {
+            filters,
+            dataRequest,
+            price,
+            initialBudgetForAudits,
+            termsAndConditions,
+            buyerURL,
+          },
+        })
         .expect(200, { rawTransaction: '0xasdasdasdasd' }, done);
     });
   });
