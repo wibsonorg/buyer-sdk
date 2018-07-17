4d107a51d0efdd38bd07d461bfe01b43c681aa6e
Daniel Fernandez <daniel@grandata.com>
2018-07-16 21:17:55 -0300

    Reading of a stream in level

--- buyer-api/src/utils/index.js
+++ buyer-api/src/utils/index.js
@@ -3,6 +3,12 @@ import cache from './cache';
 import logger from './logger';
 
 export { web3, cache, logger };
-export { createRedisStore, createLevelStore } from './storage';
+export {
+  createRedisStore,
+  createLevelStore,
+  listLevelPairs,
+  listLevelKeys,
+  listLevelValues,
+} from './storage';
 export { errorHandler, asyncError, validateAddress } from './routes';
 export { wibcoin, dataExchange, DataOrderContract } from './contracts';
