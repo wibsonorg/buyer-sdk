19c1751cc338910b670704eb5102e3c537e2d3ac
Martin Manelli <manelli.ml@gmail.com>
2018-07-17 16:44:14 -0300

    fixes

--- buyer-api/src/facades/dataResponseFacade.js
+++ buyer-api/src/facades/dataResponseFacade.js
@@ -1,8 +1,44 @@
+import web3Utils from 'web3-utils';
 import web3 from '../utils/web3';
 import signingService from '../services/signingService';
+import { getElements } from './helpers';
+import { getDataResponse } from '../utils/wibson-lib/storages';
 
-const addDataResponse = async (order, seller, notary, dataHash, signature) => {
-  const params = { order, seller, notary, dataHash, signature };
+const addDataResponse = async (dataOrderContract, order, seller) => {
+  if (!web3Utils.isAddress(order) || !web3Utils.isAddress(seller)) {
+    throw new Error('Invalid order|seller address');
+  }
+
+  const dataOrder = await dataOrderContract.at(order);
+
+  if (dataOrder.hasSellerBeenAccepted(seller)) {
+    throw new Error('Data Response has already been added');
+  }
+
+  let dataResponse;
+  try {
+    dataResponse = await getDataResponse(dataOrder, seller);
+  } catch (err) {
+    throw new Error('Could not retrieve data response from storage');
+  }
+
+  const { notaryAccount, dataHash, signature } = dataResponse;
+  if (!(web3Utils.isAddress(notaryAccount) || dataHash || signature)) {
+    throw new Error('Invalid data response payload');
+  }
+
+  const dataOrderNotaries = await getElements(dataOrder, 'notaries');
+  if (!dataOrderNotaries.includes(notaryAccount)) {
+    throw new Error('Invalid notary');
+  }
+
+  const params = {
+    order,
+    seller,
+    notary: notaryAccount,
+    dataHash,
+    signature,
+  };
 
   const { address } = await signingService.getAccount();
   const nonce = await web3.eth.getTransactionCount(address);
--- buyer-api/src/routes/dataResponses.js
+++ buyer-api/src/routes/dataResponses.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import web3Utils from 'web3-utils';
 import { asyncError } from '../utils';
 import { addDataResponse } from '../facades';
-import { getDataResponse } from '../utils/wibson-lib/storages';
 
 const router = express.Router();
 
@@ -21,35 +19,10 @@ router.post('/', asyncError(async (req, res) => {
   const { orderAddress, sellerAddress } = req.body;
   const { contracts: { DataOrderContract } } = req.app.locals;
 
-  if (!web3Utils.isAddress(orderAddress) || !web3Utils.isAddress(sellerAddress)) {
-    throw new Error('Invalid order|seller address');
-  }
-
-  const dataOrder = await DataOrderContract.at(orderAddress);
-
-  if (dataOrder.hasSellerBeenAccepted(sellerAddress)) {
-    throw new Error('Data Response has already been added');
-  }
-
-  const dataResponse = await getDataResponse(dataOrder, sellerAddress) || {};
-
-  const { notaryAccount, dataHash, signature } = dataResponse;
-  if (!(web3Utils.isAddress(notaryAccount) || dataHash || signature)) {
-    throw new Error('Invalid data response payload');
-  }
-
-  if (!dataOrder.notaries.includes(notaryAccount)) {
-    throw new Error('Invalid notary');
-  }
-
-  // 1) Call Buyer SS to create a signed transaction for adding the data response
-  // 2) Then write it to the blockchain.
-  const addDataResponseTx = await addDataResponse(
+  await addDataResponse(
+    DataOrderContract,
     orderAddress,
     sellerAddress,
-    notaryAccount,
-    dataHash,
-    signature,
   );
 
   // 3) Despues le pego al notario para que me pase el certificado y su veredicto.
