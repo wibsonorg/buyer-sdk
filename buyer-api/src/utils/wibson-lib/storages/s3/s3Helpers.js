67b0403f08a713a7162bceca78f6d47f2bce1e2e
Martin Manelli <manelli.ml@gmail.com>
2018-07-17 18:11:07 -0300

    fix s3 lib

--- buyer-api/src/utils/wibson-lib/storages/s3/s3Helpers.js
+++ buyer-api/src/utils/wibson-lib/storages/s3/s3Helpers.js
@@ -4,23 +4,23 @@ import config from '../../../../../config';
 const { storageUrl } = config;
 
 const countObjects = async (dataOrder, prefix) => {
-  const objects = await getS3Objects(storageUrl, dataOrder.orderAddress, prefix, true);
+  const objects = await getS3Objects(storageUrl, dataOrder.address, prefix, true);
   return objects.length;
 };
 
 const listObjects = (dataOrder, prefix) => {
-  return getS3Objects(storageUrl, dataOrder.orderAddress, prefix, false);
+  return getS3Objects(storageUrl, dataOrder.address, prefix, false);
 };
 
 const getObject = (dataOrder, prefix, senderAccount) => {
-  return getS3Object(storageUrl, dataOrder.orderAddress, `${prefix}${senderAccount}.json`);
+  return getS3Object(storageUrl, dataOrder.address, `${prefix}${senderAccount}.json`);
 };
 
 const sendObject = (dataOrder, prefix, senderAccount, obj) => {
   const filename = `${prefix}${senderAccount}.json`;
   const stringObject = JSON.stringify(obj);
 
-  return putS3Object(storageUrl, dataOrder.orderAddress, filename, stringObject);
+  return putS3Object(storageUrl, dataOrder.address, filename, stringObject);
 };
 
 const buildPrefixWithTarget = (prefix, target) => `${prefix}${target}-`;
