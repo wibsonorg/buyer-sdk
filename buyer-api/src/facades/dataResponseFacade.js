ede3dbffe1b9428df3833a3a1436670b8c362370
Martin Manelli <manelli.ml@gmail.com>
2018-07-18 20:35:06 -0300

    Get audit result from notary url

--- buyer-api/src/facades/dataResponseFacade.js
+++ buyer-api/src/facades/dataResponseFacade.js
@@ -1,9 +1,33 @@
 import web3Utils from 'web3-utils';
+import client from 'request-promise-native';
 import web3 from '../utils/web3';
 import signingService from '../services/signingService';
 import { getElements } from './helpers';
 import { getDataResponse } from '../utils/wibson-lib/storages';
 import { dataExchange, DataOrderContract } from '../utils';
+
+const auditResult = async (notaryUrl, order, seller, buyer) => {
+  const auditUrl = `${notaryUrl}/buyers/audit/result/${order}/${buyer}`;
+  const payload = { dataResponses: [{ seller }] };
+  const response = await client.post(auditUrl, { json: payload, timeout: 1000 });
+  const res = response.dataResponses[0];
+
+  if (res.result === 'in-progress') {
+    throw new Error('Audit result is in progress');
+  }
+
+  const wasAudited = res.result === 'success' || res.result === 'failure';
+  const isDataValid = res.result === 'success';
+  const notarySignature = res.signature;
+
+  return {
+    orderAddr: order,
+    seller,
+    wasAudited,
+    isDataValid,
+    notarySignature,
+  };
+};
 
 const addDataResponse = async (order, seller) => {
   if (!web3Utils.isAddress(order) || !web3Utils.isAddress(seller)) {
@@ -69,15 +93,7 @@ const closeDataResponse = async (order, seller) => {
   const notaryInfo = dataExchange.getNotaryInfo(notaryAddress);
   const notaryURL = notaryInfo[2];
 
-  const { wasAudited, isDataValid, notarySignature } = { wasAudited: false, isDataValid: false, notarySignature: '' };
-
-  const params = {
-    orderAddr: order,
-    seller,
-    wasAudited,
-    isDataValid,
-    notarySignature,
-  };
+  const params = await auditResult(notaryURL, order, seller, dataOrder.buyer());
 
   const { address } = await signingService.getAccount();
   const nonce = await web3.eth.getTransactionCount(address);
