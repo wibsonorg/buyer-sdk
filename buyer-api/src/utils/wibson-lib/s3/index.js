ba4e66985ade3e423256d0a272efcd5b897b6212
Nico <ncls.ayala@gmail.com>
2018-08-15 18:29:40 -0300

    Remove code not needed

--- buyer-api/src/utils/wibson-lib/s3/index.js
+++ buyer-api/src/utils/wibson-lib/s3/index.js
@@ -11,9 +11,6 @@ const client = getS3Client(
   storage.password,
 );
 
-const putS3Object = async (objectName, payload) =>
-  client.putObject(objectName, JSON.stringify(payload));
-
 const getS3Object = async (objectName) => {
   const obj = await client.getObject(objectName);
   const data = obj.Body.toString();
@@ -54,11 +51,6 @@ const getObject = (dataOrder, seller, type) => {
   return getS3Object(name);
 };
 
-const putObject = (dataOrder, seller, type, payload) => {
-  const name = `${prefix}/${dataOrder.address}/${type}/${seller}.json`;
-  return putS3Object(name, payload);
-};
-
 // //
 
 const countDataResponses = dataOrder => countObjects(dataOrder, 'data-responses');
@@ -66,8 +58,6 @@ const countDataResponses = dataOrder => countObjects(dataOrder, 'data-responses'
 const listDataResponses = dataOrder => listObjects(dataOrder, 'data-responses');
 
 const getDataResponse = (dataOrder, seller) => getObject(dataOrder, seller, 'data-responses');
-
-const putDataResponse = (dataOrder, seller, payload) => putObject(dataOrder, seller, 'data-responses', payload);
 
 const countData = dataOrder => countObjects(dataOrder, 'data');
 
@@ -79,7 +69,6 @@ export {
   countDataResponses,
   listDataResponses,
   getDataResponse,
-  putDataResponse,
   countData,
   listData,
   getData,
