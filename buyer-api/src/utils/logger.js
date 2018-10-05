51ed05f7a74663cb752afc29f79a42ea9d5a8f75
Martin Manelli <manelli.ml@gmail.com>
2018-10-05 17:11:26 -0300

    Slack notification logger

--- buyer-api/src/utils/logger.js
+++ buyer-api/src/utils/logger.js
@@ -1,12 +1,16 @@
 import winston from 'winston';
+import SlackTransport from 'winston-slack-webhook-transport';
 import config from '../../config';
 
-const logger = winston.createLogger();
+const logger = winston.createLogger({
+  levels: winston.config.syslog.levels,
+});
 
 if (config.env === 'production') {
   logger.add(new winston.transports.Console());
   logger.add(new winston.transports.File({ filename: config.log.combined }));
   logger.add(new winston.transports.File({ filename: config.log.error, level: 'error' }));
+  logger.add(new SlackTransport({ level: 'crit', webhookUrl: config.log.slack }));
 } else if (config.env === 'development') {
   logger.add(new winston.transports.Console({ format: winston.format.simple(), level: 'debug' }));
 } else {
