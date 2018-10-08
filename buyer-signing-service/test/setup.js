be7447f84551dd9754853577b7cacb93009de15c
Nico <ncls.ayala@gmail.com>
2018-10-08 12:11:19 -0300

    Add specs for new endpoints

---
+++ buyer-signing-service/test/setup.js
@@ -0,0 +1,6 @@
+// TODO: This is a temporal solution, this file should not exist
+process.env.CHAIN_ID = 9697;
+process.env.WIBCOIN_ADDRESS = '0xd518cd50e83b8c3fe28543a287fc00c0bc8c9de1';
+process.env.DATA_EXCHANGE_ADDRESS = '0xf3b435d66a6156622e1b3c1a974d25cdbf6032aa';
+process.env.TX_TRANSFER_GAS_LIMIT = 100000;
+process.env.BUYER_SEED = 'hello';
