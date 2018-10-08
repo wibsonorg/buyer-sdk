ecd2d7546917f10841a441156c455fd7b3fb462b
Nico <ncls.ayala@gmail.com>
2018-10-08 12:31:03 -0300

    Fix tests

--- buyer-signing-service/test/routes/sign/closeOrderSpec.js
+++ buyer-signing-service/test/routes/sign/closeOrderSpec.js
@@ -1,17 +1,11 @@
 import request from 'supertest';
-import config from '../../../config';
 import app from '../../../src/app';
 
 describe('/sign/close-order', () => {
   const nonce = 0;
   const orderAddr = '0xa662a5c63079009d79740f4e638a404f7917f93a';
-
-  beforeEach(() => {
-    config.contracts.chainId = 9697;
-    config.contracts.dataExchange.address = '0xf3b435d66a6156622e1b3c1a974d25cdbf6032aa';
-    config.contracts.dataExchange.closeOrder.gasLimit = 2000000;
-    config.buyer.privateKey = '123fa47078166dd487b92f856bfb4685dac280f486670248267450f10062f6e8';
-  });
+  const gasPrice = 1000000000; // 1 gwei
+  const account = { number: 0 };
 
   describe('POST /', () => {
     it('responds with an Unprocessable Entity status when nonce is not present');
@@ -20,8 +14,15 @@ describe('/sign/close-order', () => {
     it('responds with an OK status', (done) => {
       request(app)
         .post('/sign/close-order')
-        .send({ nonce, orderAddr })
-        .expect(200, { signedTransaction: 'f8848080831e848094f3b435d66a6156622e1b3c1a974d25cdbf6032aa80a490e72127000000000000000000000000a662a5c63079009d79740f4e638a404f7917f93a1ca0ea5ead0993f3032a3764c6cf8ae690d2e3a8974bdd91e263c6c7afcaae29763ca059f8f94e5e477b57aff971eb526be73303063b0928c7712a3b53c38c451e3ad4' }, done);
+        .send({
+          account,
+          gasPrice,
+          nonce,
+          params: {
+            orderAddr,
+          },
+        })
+        .expect(200, { signedTransaction: 'f88780843b9aca008094f3b435d66a6156622e1b3c1a974d25cdbf6032aa80a490e72127000000000000000000000000a662a5c63079009d79740f4e638a404f7917f93a824be6a02becd9d580e3a7c0368cb6c1751d8a3711d80f5e985d921627dc4a9e247a91c2a0172625dbaa084b83a116aed9acac440d49c6c1fbae1bdf92777f4f591f0cb635' }, done);
     });
   });
 });
