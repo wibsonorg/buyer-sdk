74a7fe9dd871934e3502eae5b5f66323a1a37dea
Daniel Fernandez <daniel@grandata.com>
2018-10-18 20:24:36 -0300

    S3 made retrocompatible

--- buyer-api/src/utils/wibson-lib/s3/getS3Client.js
+++ buyer-api/src/utils/wibson-lib/s3/getS3Client.js
@@ -8,29 +8,35 @@ class AWSWrapper {
 
   promisify(methodName, params) {
     return new Promise((res, rej) =>
-      this.s3[methodName](
-        { Bucket: this.bucket, ...params },
-        (err, data) => (err ? rej : res)(err || data),
-      ));
+      this.s3[methodName]({ Bucket: this.bucket, ...params }, (err, data) =>
+        (err ? rej : res)(err || data)));
   }
 
   getObject(key) {
-    return this.promisify('getObject', { Key: key.toLowerCase() });
+    return this.promisify('getObject', { Key: key.toLowerCase() }).catch(() =>
+      this.promisify('getObject', { Key: key })); // making it retrocompatible
   }
+
   putObject(key, obj) {
     return this.promisify('putObject', { Key: key.toLowerCase(), Body: obj });
   }
 
-  async listObjects(prefix) {
+  async listObjectsAsIs(prefix) {
     const objects = [];
     let ContinuationToken;
     do {
-      const params = { Prefix: prefix.toLowerCase(), ContinuationToken };
+      const params = { Prefix: prefix, ContinuationToken };
       const batch = await this.promisify('listObjectsV2', params); // eslint-disable-line no-await-in-loop
       objects.push(...batch.Contents);
       ContinuationToken = batch.IsTruncated && batch.NextContinuationToken;
     } while (ContinuationToken);
     return objects;
+  }
+
+  async listObjects(prefix) { // making it retrocompatible
+    const lowerCaseObjects = await this.listObjectsAsIs(prefix.toLowerCase());
+    const objects = await this.listObjectsAsIs(prefix);
+    return [...lowerCaseObjects, ...objects];
   }
 }
 
@@ -39,12 +45,15 @@ const getS3Client = (uri, region, bucket, accessKeyId, secretAccessKey) => {
   if (!region) throw new Error('A Region is required');
   if (!bucket) throw new Error('A Bucket is required');
 
-  return new AWSWrapper(bucket, new aws.S3({
-    accessKeyId,
-    secretAccessKey,
-    region,
-    s3ForcePathStyle: true,
-  }));
+  return new AWSWrapper(
+    bucket,
+    new aws.S3({
+      accessKeyId,
+      secretAccessKey,
+      region,
+      s3ForcePathStyle: true,
+    }),
+  );
 };
 
 export default getS3Client;
