df7eb0ea57539cb29b0b63121dc94785bce6cb11
Nico <ncls.ayala@gmail.com>
2018-10-30 16:42:42 -0300

    Add notariesCache to utils/stores.js

--- buyer-api/src/utils/stores.js
+++ buyer-api/src/utils/stores.js
@@ -1,7 +1,8 @@
-import { createLevelStore } from './storage';
+import { createLevelStore, createRedisStore } from './storage';
 
 const eventBlocks = createLevelStore('event_blocks');
 const buyerInfos = createLevelStore('buyer_infos');
 const buyerInfoPerOrder = createLevelStore('buyer_info_per_order');
+const notariesCache = createRedisStore('notaries.cache');
 
-export { eventBlocks, buyerInfos, buyerInfoPerOrder };
+export { eventBlocks, buyerInfos, buyerInfoPerOrder, notariesCache };
