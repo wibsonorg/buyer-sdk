93d7c220eba5dd03395ccae4130e1dc26a609b90
Agustin Modugno <agustin.modugno@grandata.com>
2018-11-06 18:12:55 -0300

    Fixing export/import coin-related

--- buyer-api/src/routes/account.js
+++ buyer-api/src/routes/account.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { web3, cache, asyncError, wibcoin, coin } from '../utils';
+import { web3, cache, asyncError, wibcoin } from '../utils';
+import { coin } from '../utils/wibson-lib';
 import signingService from '../services/signingService';
 
 const router = express.Router();
@@ -21,14 +22,14 @@ router.get('/', cache('30 seconds'), asyncError(async (req, res) => {
   const { root, children } = await signingService.getAccounts();
 
   const tokenBalancePromises = children.concat(root)
-    .map(({ address }) => wibcoin.balanceOf.call(address));
+    .map(({ address }) => wibcoin.methods.balanceOf(address).call());
   const tokenBalances = await Promise.all(tokenBalancePromises);
-  const tokenBalance = tokenBalances.reduce((accum, item) => item.plus(accum), 0);
+  const tokenBalance = tokenBalances.reduce((accum, item) => Number(item) + accum, 0);
 
   const ethBalancePromises = children.concat(root)
     .map(({ address }) => web3.eth.getBalance(address));
   const ethBalances = await Promise.all(ethBalancePromises);
-  const ethBalance = ethBalances.reduce((accum, item) => item.plus(accum), 0);
+  const ethBalance = ethBalances.reduce((accum, item) => Number(item) + accum, 0);
 
   const ether = web3.utils.fromWei(ethBalance.toString(), 'ether');
   const wib = coin.formatWib(coin.toWib(tokenBalance).toString(10), { decimals: 2 });
