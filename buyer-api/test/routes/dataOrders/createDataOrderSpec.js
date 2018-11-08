99a882e44debe2e967b051d32842ed1258500385
Nico <ncls.ayala@gmail.com>
2018-11-08 14:32:39 -0300

    Fix failing tests

--- buyer-api/test/routes/dataOrders/createDataOrderSpec.js
+++ buyer-api/test/routes/dataOrders/createDataOrderSpec.js
@@ -1,8 +1,12 @@
 import request from 'supertest';
+import { storeBuyerInfo } from '../../../src/services';
+import web3 from '../../../src/utils/web3';
 import { mockStorage, restoreMocks, requireApp } from '../../helpers';
 
 describe('/orders', () => {
   let app;
+  const buyerInfoId = 'social-good';
+  let notaries = [];
 
   const dataOrder = {
     filters: {
@@ -13,13 +17,28 @@ describe('/orders', () => {
     price: 20,
     initialBudgetForAudits: 10,
     termsAndConditions: 'DataOrder T&C',
+    buyerURL: 'https://buyer.example.com/data',
+    buyerInfoId,
+    notaries,
   };
 
   beforeEach(function (done) { // eslint-disable-line func-names
     this.timeout(5000);
     mockStorage();
     app = requireApp();
-    done();
+
+    web3.eth.getAccounts().then(res => { notaries = [res[5], res[6]] });
+    storeBuyerInfo(buyerInfoId, {
+      id: buyerInfoId,
+      label: 'Social Good',
+      description: 'Social Good $ Research',
+      category: {
+        id: 'research-social-good',
+        label: 'Social Good & Research',
+        description: 'Social Good & more Research',
+      },
+      terms: '# WIBSON ALPHA TERMS OF USE',
+    }).then(done);
   });
 
   afterEach(() => {
@@ -33,19 +52,18 @@ describe('/orders', () => {
     it('responds with an Unprocessable Entity status when initialBudgetForAudits is not present');
     it('responds with an Unprocessable Entity status when termsAndConditions is not present');
 
-    // eslint-disable-next-line func-names
-    it('responds with an OK status', function (done) {
-      this.timeout(60 * 1000);
-
+    it('responds with an OK status', (done) => {
       request(app)
-        .post('/orders')
-        .send({
-          dataOrder: {
-            ...dataOrder,
-            buyerURL: 'https://buyer.example.com/data',
-          },
-        })
-        .expect(200, done);
+        .post('/authentication')
+        .send({ password: 'pass' })
+        .expect(200)
+        .then((response) => {
+          request(app)
+            .post('/orders')
+            .set('Authorization', `Bearer ${response.body.token}`)
+            .send({ dataOrder })
+            .expect(200, done);
+        });
     });
   });
 });
