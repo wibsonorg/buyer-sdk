4ba796806ac4e1c72af7aacfd6277604ed26873c
Agustin Modugno <agustin.modugno@grandata.com>
2018-11-08 15:44:17 -0300

    Update to web3 1.0 method

--- buyer-api/src/facades/helpers/getBuyerAccount.js
+++ buyer-api/src/facades/helpers/getBuyerAccount.js
@@ -1,7 +1,7 @@
 import signingService from '../../services/signingService';
 
 export const getBuyerAccount = async (dataOrder) => {
-  const rawFilters = dataOrder.filters();
+  const rawFilters = await dataOrder.methods.filters().call();
   const filters = JSON.parse(rawFilters);
   const { ethAddress } = filters;
   return ethAddress && signingService.getAccount(ethAddress.bucketNumber);
