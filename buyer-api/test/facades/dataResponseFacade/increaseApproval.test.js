b653699516beb5000e21dc7ad2fc964934ff1085
Agustin Modugno <agustin.modugno@grandata.com>
2018-11-09 17:29:06 -0300

    Adding one test and env configuration

--- buyer-api/test/facades/dataResponseFacade/increaseApproval.test.js
+++ buyer-api/test/facades/dataResponseFacade/increaseApproval.test.js
@@ -1,4 +1,19 @@
 import test from 'ava';
-// import { checkAllowance } from '../../../src/facades';
-test.skip('sends tx if minimumAllowance is less than config.allowance', t => t.fail());
-test.skip('does not send tx if minimumAllowance is greater than config.allowance', t => t.fail());
+import sinon from 'sinon';
+import { checkAllowance } from '../../../src/facades';
+// import { enqueueTransaction } from '../../../src/queues';
+
+// let app;
+test.before(async () => {
+  // app = require('../../../src/app'); // eslint-disable-line global-require
+});
+
+test('sends tx if allowance is less than config.minimumAllowance', async (assert) => {
+  const callback = sinon.spy();
+  await checkAllowance(callback);
+  assert.is(callback.called, true);
+});
+
+test.skip('does not send tx if allowance is greater than config.minimumAllowance', async (assert) => {
+  assert.fail();
+});
