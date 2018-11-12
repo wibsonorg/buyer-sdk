d0bc94c93c4195e9c0733da296dedcc66af76749
Agustin Modugno <agustin.modugno@grandata.com>
2018-11-12 18:45:14 -0300

    Adding one functional test to addDataResponse

--- buyer-api/test/facades/dataResponseFacade/addDataResponse.test.js
+++ buyer-api/test/facades/dataResponseFacade/addDataResponse.test.js
@@ -1,48 +1,33 @@
 import test from 'ava';
-import { enqueueTransaction, priority } from '../../../src/queues';
-import signingService from '../../../src/services/signingService';
-import config from '../../../config';
+import sinon from 'sinon';
+// import signingService from '../../../src/services/signingService';
+import * as utils from '../../../src/utils'; // { dataOrderAt }
+import * as s3 from '../../../src/utils/wibson-lib/s3'; // { getDataResponse }
 // import request from 'supertest';
-// import { addDataResponse } from '../../../src/facades';
-
-const buyerInfoId = 'social-good';
-const notaries = ['0x8b64c30a41dbe0ffc74f0cc601bb50a6151eb0c1'];
-let transaction = {};
-
-const dataOrder = {
-  filters: {
-    age: '30..35',
-    gender: 'male',
-  },
-  dataRequest: 'data request',
-  price: 20,
-  initialBudgetForAudits: 10,
-  termsAndConditions: 'DataOrder T&C',
-  buyerURL: 'https://buyer.example.com/data',
-  buyerInfoId,
-  notaries,
-};
+import { addDataResponse } from '../../../src/facades';
 
 test.before(async () => {
-  const account = await signingService.getAccount();
-  const job = await enqueueTransaction(
-    account,
-    'NewOrder',
-    dataOrder,
-    config.contracts.gasPrice.fast,
-    {
-      priority: priority.HIGH,
+  sinon.stub(utils, 'dataOrderAt').callsFake(() => ({
+    methods: {
+      hasSellerBeenAccepted: () => ({
+        call: () => 1,
+      }),
     },
-  );
-  transaction = await job.finished();
+  }));
+  sinon.stub(s3, 'getDataResponse').callsFake(() => ({
+    notaryAccount: '0x',
+    dataHash: '0x',
+    signature: '0x',
+  }));
+  // const account = await signingService.getAccount();
 });
 
-test('responds with error when orderAddress is invalid', (assert) => {
-  console.log({ transaction });
-  assert.pass();
-  /* filters, dataRequest, price, initialBudgetForAudits, buyerURL, notaries, buyerInfoId, */
-// await addDataResponse(order, seller, enqueueTransaction)
+test('responds with error when orderAddress is invalid', async (assert) => {
+  // order, seller, enqueueTransaction
+  const error = await assert.throws(addDataResponse('I will fail!', undefined, undefined));
+  assert.is(error.message, 'Invalid order|seller address');
 });
+
 test.skip('responds with error when sellerAddress is invalid', t => t.fail());
 test.skip('responds true if seller has already accepted', t => t.fail());
 test.skip('throws if it cannot retrieve data response from storage', t => t.fail());
