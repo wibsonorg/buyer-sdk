18e6530923b7f40fc6d783e2014518e3a58dba40
Agustin Modugno <agustin.modugno@grandata.com>
2018-11-12 13:26:52 -0300

    Running test serially since I'm stubbing a value

--- buyer-api/test/facades/dataResponseFacade/increaseApproval.test.js
+++ buyer-api/test/facades/dataResponseFacade/increaseApproval.test.js
@@ -1,19 +1,18 @@
 import test from 'ava';
 import sinon from 'sinon';
 import { checkAllowance } from '../../../src/facades';
-// import { enqueueTransaction } from '../../../src/queues';
+import config from '../../../config';
 
-// let app;
-test.before(async () => {
-  // app = require('../../../src/app'); // eslint-disable-line global-require
-});
-
-test('sends tx if allowance is less than config.minimumAllowance', async (assert) => {
+test.serial('sends tx if allowance is less than config.minimumAllowance', async (assert) => {
   const callback = sinon.spy();
   await checkAllowance(callback);
   assert.is(callback.called, true);
 });
 
-test.skip('does not send tx if allowance is greater than config.minimumAllowance', async (assert) => {
-  assert.fail();
+test.serial('does not send tx if allowance is greater than config.minimumAllowance', async (assert) => {
+  const minimumAllowanceStub = sinon.stub(config.allowance, 'minimumAllowance').value('-1');
+  const callback = sinon.spy();
+  await checkAllowance(callback);
+  assert.is(callback.called, false);
+  minimumAllowanceStub.restore();
 });
