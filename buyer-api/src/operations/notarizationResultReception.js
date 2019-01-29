f325ae2fdf68a234a0e772790888518c741be32b
pabloaguayo <pablo.aguayo@soluciones3f.com.ar>
2019-01-29 18:29:26 -0300

    refactor notarizationResultReception

--- buyer-api/src/operations/notarizationResultReception.js
+++ buyer-api/src/operations/notarizationResultReception.js
@@ -2,16 +2,14 @@
 import { addTransactionJob } from '../queues/transactionQueue';
 
 export function notarizationResultReception(notarizationRequest, notarizationResult) {
+  // validate input 
+  // notarizationRequest.sellers matches notarizationResult.sellers (address field)
+  // avoid duplicated addresses, avoid not requested addresses
   notarizationResult.sellers = notarizationResult.sellers.filter((seller, index, self) =>
     index === self.findIndex((s) => (
       s.address === seller.address
-    ))
+    )) && notarizationRequest.sellers.map(y => y.address).includes(seller.address)
   );
 
-  // validate input notarizationRequest.sellers matches notarizationResult.sellers (address field)
-  let filteredSellers = notarizationResult.sellers.filter(x => 
-    notarizationRequest.sellers.map(y => y.address).includes(x.address));
-
-  notarizationResult.sellers = filteredSellers
   return addTransactionJob('TranferNotarizationResult', notarizationResult);
 }
