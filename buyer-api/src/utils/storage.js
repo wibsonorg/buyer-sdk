3e4a31e349c3a6ce9bb439a8e0f8eeeb8fc5d68f
Nahuel Santoalla <nahjersan@gmail.com>
2019-01-30 19:08:05 -0300

    add utils/storage jsdocs

--- buyer-api/src/utils/storage.js
+++ buyer-api/src/utils/storage.js
@@ -16,14 +16,36 @@ const listLevelStream = stream =>
       .on('end', () => resolve(result));
   });
 
+/**
+ * @typedef LevelStore A store that uses LevelDB
+ * @property {function(K):Promise<V>} fetch Retrives the value parsed from LevelDB
+ * @property {function(K):Promise<V>} safeFetch As fetch but retrives null if missing
+ * @property {function(K,V):Promise<void>} store Stores the value stringified on LevelDB
+ * @property {function():Promise<K[]>} listKeys Lists all the keys stored on LevelDB
+ * @property {function():Promise<V[]>} listValues Lists all the values stored on LevelDB
+ * @property {function():Promise<({id:K}&V)[]>} list Lists all the objects stored on LevelDB
+ * @template K
+ * @template V
+ */
+
+/**
+ * @function createLevelStore Creates a level store
+ * @param {string} dir Path to the store, starting on {config.levelDirectory}
+ * @returns {LevelStore} A store that uses LevelDB with the given path
+ */
 export const createLevelStore = (dir) => {
   const store = level(`${config.levelDirectory}/${dir}`, (err, db) => {
-    if (err) {
-      throw new Error(err);
-    }
+    if (err) { throw new Error(err); }
     return db;
   });
   store.fetch = async id => JSON.parse(await store.get(id));
+  store.safeFetch = async (id) => {
+    try {
+      return await store.fetch(id);
+    } catch (_) {
+      return null;
+    }
+  };
   store.store = (id, payload) => store.put(id, JSON.stringify(payload));
   store.listKeys = () => listLevelStream(store.createKeyStream());
   store.listValues = async () => {
--- buyer-api/src/utils/stores.js
+++ buyer-api/src/utils/stores.js
@@ -1,7 +1,10 @@
 import { createLevelStore, createRedisStore } from './storage';
 
+/** @type {import('./storage').LevelStore<string,DataOrder>} */
 export const dataOrders = createLevelStore('data_orders');
+/** @type {import('./storage').LevelStore<string,number>} */
 export const eventBlocks = createLevelStore('event_blocks');
+/** @type {import('./storage').LevelStore<string,BuyerInfo>} */
 export const buyerInfos = createLevelStore('buyer_infos');
 export const buyerInfoPerOrder = createLevelStore('buyer_info_per_order');
 export const notariesCache = createRedisStore('notaries.cache');
