d6cbb37044e3bbb94ab319f2277b8c4758650304
Facundo D <genuinefafa@gmail.com>
2019-01-30 16:55:27 -0300

    +jsdoc +fixtures +lint +refactor test, tidy code

--- buyer-api/src/operations/notarizationResultReception.js
+++ buyer-api/src/operations/notarizationResultReception.js
@@ -1,15 +1,34 @@
-
 import { addTransactionJob } from '../queues/transactionQueue';
 
+/**
+ * Seller inside Notarization Request
+ * @typedef {Object} SellersInNotarizationRequest
+ * @property {NumberLike} id - numeric id for the seller
+ * @property {uuid} address - has for the seller
+ * @property {KeyHash} decryptionKeyHash - decryption key for seller data
+ */
+
+/**
+ * Requeriment for notarization
+ * @typedef {Object} NotarizationRequest
+ * @property {NumberLike} orderId - tracking Id for the order
+ * @property {String} callbackUrl - callback url for the notarization result
+ * @property {SellersInNotarizationRequest[]} sellers - list of sellers
+ */
+
+/**
+ * take notarization result, validate and enqueue for transfer
+ * @param {NotarizationRequest} notarizationRequest original request sent for notarization
+ * @param {NotarizationResult} notarizationResult results related with request done by notary
+ */
 export function notarizationResultReception(notarizationRequest, notarizationResult) {
-  // validate input 
+  // validate input
   // notarizationRequest.sellers matches notarizationResult.sellers (address field)
   // avoid duplicated addresses, avoid not requested addresses
-  notarizationResult.sellers = notarizationResult.sellers.filter((seller, index, self) =>
-    index === self.findIndex((s) => (
+  const sellers = notarizationResult.sellers.filter((seller, index, self) =>
+    index === self.findIndex(s => (
       s.address === seller.address
-    )) && notarizationRequest.sellers.map(y => y.address).includes(seller.address)
-  );
+    )) && notarizationRequest.sellers.map(y => y.address).includes(seller.address));
 
-  return addTransactionJob('TranferNotarizationResult', notarizationResult);
+  return addTransactionJob('TranferNotarizationResult', sellers);
 }
---
+++ buyer-api/test/operations/notarizationResultReception.fixture.js
@@ -0,0 +1,60 @@
+export const someNotarizationResult = {
+  sellers: [
+    {
+      id: 78,
+      address: '0x338fff484061da07323e994990c901d322b6927a',
+      result: 'ok',
+      decryptionKeyEncryptedWithMasterKey: '0x912f8f484454e3a38f7535fbf6b7f0035a0fe27c028163348965eb9369fcca8c',
+    },
+    {
+      id: 84,
+      address: '0x2d419c641352e0baa7f54328ecabf58c5e4a56f1',
+      result: 'not_audited',
+      decryptionKeyEncryptedWithMasterKey: '0x912f8f484454e3a38f7535fbf6b7f0035a0fe27c028163348965eb9369fcca8c',
+    },
+  ],
+  orderId: 114,
+  notaryAddress: '0xfe174860ad53e45047BABbcf4aff735d650D9284',
+  notarizationPercentage: 30,
+  notarizationFee: 10000000000,
+  payDataHash: '0x0bb68ec2b34b7b611727f7340d7c6e0ee5a580090583d92b7639861802b9e116',
+  lock: '0xde916ce0390bd5408b7a0a52aae818fd973858c7e9b5d368ec1e6a9b0db44cf9',
+};
+
+export const someNotarizationResultWithNonRequestedAddresses = { ...someNotarizationResult };
+someNotarizationResultWithNonRequestedAddresses.sellers =
+  someNotarizationResultWithNonRequestedAddresses.sellers.slice(0);
+someNotarizationResultWithNonRequestedAddresses.sellers.push({
+  id: 94,
+  address: '0x2d419c641351e0baa7f54328ecabf58c5e4a56f1',
+  result: 'not_audited',
+  decryptionKeyEncryptedWithMasterKey: '0x912f8f484454e3a38f7535fbf6b7f0035a0fe27c028163348965eb9369fcca8c',
+});
+
+export const someNotarizationResultWithDuplicatedAddresses = { ...someNotarizationResult };
+someNotarizationResultWithDuplicatedAddresses.sellers =
+  someNotarizationResultWithDuplicatedAddresses.sellers.slice(0);
+someNotarizationResultWithDuplicatedAddresses.sellers.push({
+  id: 78,
+  address: '0x338fff484061da07323e994990c901d322b6927a',
+  result: 'ok',
+  decryptionKeyEncryptedWithMasterKey: '0x912f8f484454e3a38f7535fbf6b7f0035a0fe27c028163348965eb9369fcca8c',
+});
+
+export const someNotarizationRequest = {
+  orderId: 114,
+  callbackUrl: 'http://api.wibson.org/notarization-result/0x87c2d362de99f75a4f2755cdaaad2d11bf6cc65dc71356593c445535ff28f43d',
+  sellers: [
+    {
+      id: 78,
+      address: '0x338fff484061da07323e994990c901d322b6927a',
+      decryptionKeyHash: '0x32f2ab2ab2157408e0d8e8af2677f628ec82b5fc9329facb8fcc71cfb8f0b92e',
+    },
+    {
+      id: 84,
+      address: '0x2d419c641352e0baa7f54328ecabf58c5e4a56f1',
+      decryptionKeyHash: '0x15e4d58cc37f15f2de3e688b4a21e5b5caa5263f9dd47db2011d8643cc59779f',
+    },
+  ],
+};
+
