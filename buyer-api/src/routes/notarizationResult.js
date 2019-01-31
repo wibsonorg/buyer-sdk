5a1532df56657f38ba8fee343cebfe2962dc00d4
pabloaguayo <pablo.aguayo@soluciones3f.com.ar>
2019-01-31 10:49:37 -0300

    +jsdoc

--- buyer-api/src/routes/notarizationResult.js
+++ buyer-api/src/routes/notarizationResult.js
@@ -11,24 +11,61 @@ const router = express.Router();
  *   post:
  *     description: 
  *     parameters:
- *       - name: notaryAddress
- *         description: Notary's ethereum address
+ *       - name: notarizationRequestId
+ *         description: Notarization request id
  *         required: true
  *         type: string
+ *         in: uri
+ *       - name: orderId
+ *         description: The unique identifier for the order.
+ *         required: true
+ *         type: number
+ *         in: body
+ *       - name: notaryAddress
+ *         description: Notary's ethereum address.
+ *         required: true
+ *         type: string
+ *         in: body
+ *       - name: notarizationPercentage
+ *         description: Percentage of orders that have been notarized.
+ *         required: true
+ *         type: number
+ *         in: body
+ *       - name: notarizationFee
+ *         description: Flat one-time payment for the notarization services.
+ *         required: true
+ *         type: number
+ *         in: body
+ *       - name: payDataHash
+ *         description: .
+ *         required: true
+ *         type: string
+ *         in: body
+ *       - name: lock
+ *         description: .
+ *         required: true
+ *         type: string
+ *         in: body
+ *       - name: sellers
+ *         description: List of sellers send to notary.
+ *         required: true
+ *         type: array
+ *         in: body
  *     produces:
  *       - application/json
  *     responses:
- *       200:
- *         description: When the notary could be fetched correctly.
+ *       202:
+ *         description: When the result is validated.
  *       404:
- *         description: When the notary is not registered in the Data Exchange.
+ *         description: When could not find the notrarization request.
+ *       422:
+ *         description: When some field is missing or incorrect.
  *       500:
  *         description: When the fetch failed.
  */
 router.post(
   '/:notarizationRequestId',
   asyncError(async (req, res) => {
-    req.apicacheGroup = '/notaries/*';
     const { notarizationRequestId } = req.params;
     const notarizationResult = req.body;
 
@@ -43,8 +80,8 @@ router.post(
     } else {
       let request = getNotarizationRequest(notarizationRequestId)
       if(!request) {
-        res.status(400).json({
-          "statusCode": 400,
+        res.status(404).json({
+          "statusCode": 404,
           "message": "Could not find a request whith the id provided"
         });
       }
