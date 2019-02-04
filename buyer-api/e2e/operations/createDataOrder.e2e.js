df8817a9c9f209bd3f95c4f8483278e058330c0f
Nahuel Santoalla <nahjersan@gmail.com>
2019-02-04 13:31:46 -0300

    longer waiting period on createDataOrder e2e

--- buyer-api/e2e/operations/createDataOrder.e2e.js
+++ buyer-api/e2e/operations/createDataOrder.e2e.js
@@ -3,33 +3,33 @@ import { api, wibsonBuyer, dataOrder } from './createDataOrder.mock';
 
 const it = test.serial;
 
-it('createThenGetDataOrder > authenticates', async (assert) => {
+it('createDataOrder > authenticates', async (assert) => {
   const res = await api.post('authentication', { password: 'passphrase' });
   assert.true(res.authenticated);
   api.setToken(res.token);
 });
 
-it('createThenGetDataOrder > has or creates buyer', async (assert) => {
+it('createDataOrder > has or creates buyer', async (assert) => {
   const { message } = await api
     .post('infos', wibsonBuyer)
     .catch(err => err.response.data);
   assert.true(['OK', 'The ID already exists'].includes(message));
 });
 
-it('createThenGetDataOrder > starts data order creation', async (assert) => {
+it('createDataOrder > starts data order creation', async (assert) => {
   const { status, id } = await api.post('orders', { dataOrder });
   dataOrder.id = id;
   assert.is(status, 'creating');
 });
 
-it('createThenGetDataOrder > gets data order with creating status', async (assert) => {
+it('createDataOrder > gets data order with creating status', async (assert) => {
   const { status } = await api.get(`orders/${dataOrder.id}`);
   assert.is(status, 'creating');
 });
 
-it('createThenGetDataOrder > waits for ganache to process the tx then gets the order with creatd status', async (assert) => {
+it('createDataOrder > waits for ganache to process the tx then gets the order with creatd status', async (assert) => {
   let status;
-  for (let i = 0; i < 30 && status !== 'created'; i += 1) {
+  for (let i = 0; i < 120 && status !== 'created'; i += 1) {
     /* eslint-disable no-await-in-loop */
     await new Promise(res => setTimeout(res, 1000));
     ({ status } = await api.get(`orders/${dataOrder.id}`));
