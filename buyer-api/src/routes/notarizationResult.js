3a5047d92e67f916017f9e24542d3f84037c6773
Nahuel Santoalla <Nahjersan@gmail.com>
2019-02-05 08:14:04 -0300

    tag improvement
    Co-Authored-By: pabloaguayo <pablo.aguayo@soluciones3f.com.ar>

--- buyer-api/src/routes/notarizationResult.js
+++ buyer-api/src/routes/notarizationResult.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * /notarization-result/:notarizationRequestId:
+ * /notarization-result/{notarizationRequestId}
  *   post:
  *     parameters:
  *       - name: notarizationRequestId
