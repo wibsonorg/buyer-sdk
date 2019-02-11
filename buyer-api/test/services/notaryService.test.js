6c8dfa374a0c5e921eeadea89eaefa3daf1e4f3a
Nico <ncls.ayala@gmail.com>
2019-02-11 18:55:25 -0300

    Fix tests and update snapshots

--- buyer-api/test/services/notaryService.test.js
+++ buyer-api/test/services/notaryService.test.js
@@ -1,8 +1,8 @@
 import { serial as it } from 'ava';
 import { addTransactionJob } from './notaryService.mock';
-import { transferNotarizacionResult } from '../../src/services/notaryService';
+import { transferNotarizationResult } from '../../src/services/notaryService';
 
 it('adds a transaction job', async (assert) => {
-  await transferNotarizacionResult('not-req-id');
+  await transferNotarizationResult('not-req-id');
   assert.snapshot(addTransactionJob.lastCall.args, { id: 'addTransactionJob().args' });
 });
