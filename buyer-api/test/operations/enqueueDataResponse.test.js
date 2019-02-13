abb7e62bc6d0bb1470b643a234759e9cdf03f50f
Nahuel Santoalla <nahjersan@gmail.com>
2019-02-13 14:20:19 -0300

    fix enqueueDataResponse test

--- buyer-api/test/operations/enqueueDataResponse.test.js
+++ buyer-api/test/operations/enqueueDataResponse.test.js
@@ -5,7 +5,7 @@ import { enqueueDataResponse } from '../../src/operations/enqueueDataResponse';
 it('returns an error when the data response does not exist', async (assert) => {
   const { error, status } = await enqueueDataResponse(2, '2', 2);
   assert.is(status, undefined);
-  assert.is(error, 'There\'s not a DataResponse whith the data provided');
+  assert.is(error, 'There\'s not a DataResponse with the data provided');
 });
 
 it('returns an status when the data response is not in `waiting`', async (assert) => {
