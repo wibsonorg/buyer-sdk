fa739dbf50c98864b0d8927b079609f42f67c61b
Daniel Fernandez <daniel@grandata.com>
2019-04-10 10:33:17 -0300

    Scripts to create builds for different environments

--- buyer-app/buyer-app/src/config/index.js
+++ buyer-app/buyer-app/src/config/index.js
@@ -7,10 +7,12 @@ if (process.env.DEPLOY_ENVIRONMENT === "development") {
   baseConf = { ...defaultConf, ...require("../../configuration/development.json") };
 } else if (process.env.DEPLOY_ENVIRONMENT === "staging") {
   baseConf = { ...defaultConf, ...require("../../configuration/staging.json")  };
-} else if (process.env.DEPLOY_ENVIRONMENT === "production") {
-  baseConf = { ...defaultConf, ...require("../../configuration/production.json")  };
-} else if (!process.env.DEPLOY_ENVIRONMENT) {
-  console.error("DEPLOY_ENVIRONMENT is not set")
+} else if (process.env.DEPLOY_ENVIRONMENT === "production-bda") {
+  baseConf = { ...defaultConf, ...require("../../configuration/production-bda.json")  };
+} else if (process.env.DEPLOY_ENVIRONMENT === "production-jampp") {
+  baseConf = { ...defaultConf, ...require("../../configuration/production-jampp.json")  };
+} else {
+  console.error("DEPLOY_ENVIRONMENT not recognized")
 }
 
 baseConf.env = process.env.DEPLOY_ENVIRONMENT;
