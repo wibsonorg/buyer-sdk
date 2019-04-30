a07cdf835dbccf57ee2979cbf994a63d50662c63
Daniel Fernandez <daniel@grandata.com>
2019-04-30 12:53:16 -0300

    Contracts events listener updated

--- buyer-api/src/blockchain/contractEventListener.js
+++ buyer-api/src/blockchain/contractEventListener.js
@@ -1,37 +1,44 @@
 import { EventEmitter } from 'events';
 import { eventBlocks } from '../utils/stores';
-import { logger } from '../utils';
+import { logger, web3 } from '../utils';
 import config from '../../config';
 
 const { lastProcessedBlock, interval } = config.contractEventListener;
 export const contractEventListener = new EventEmitter();
 
 const contracts = [];
-const processEvents = async () => {
-  // getStartingBlock
-  const fromBlock = await eventBlocks.get('last_processed_block')
-    .then(lastBlock => Number(lastBlock) + 1)
-    .catch(() => lastProcessedBlock);
-  logger.debug(`Contract Events :: From block :: ${fromBlock}`);
-  // getEvents
-  const allEvents = contracts.map(c => c.getPastEvents('allEvents', { fromBlock }));
-  const confirmedEvents = (await Promise.all(allEvents))
-    .reduce((acc, val) => acc.concat(val), [])
-    .filter(result => Number(result.blockNumber) > 0);
-  // emitEvents
-  confirmedEvents.forEach((e) => {
-    logger.info(`Contract Events :: Processing Event :: '${e.event}'`);
-    contractEventListener.emit(e.event, e.returnValues, e);
+const processEvents = () =>
+  eventBlocks.update('last_processed_block', async (lastBlock = lastProcessedBlock) => {
+    const fromBlock = Number(lastBlock) + 1;
+    const toBlock = await web3.eth.getBlockNumber();
+    logger.debug(`Contract Events :: Blocks ${fromBlock} to ${toBlock}`);
+
+    const allEvents = contracts.map(c => c.getPastEvents('allEvents', { fromBlock, toBlock }));
+    const confirmedEvents = (await Promise.all(allEvents))
+      .reduce((acc, val) => acc.concat(val), [])
+      .filter(result => Number(result.blockNumber) > 0);
+
+    logger.debug(`Contract Events :: Events to process :: ${confirmedEvents.length}`);
+
+    const proccessData = { fromBlock, toBlock, events: confirmedEvents };
+    // Pre-announce events
+    contractEventListener.emit('ProcessingEvents', proccessData);
+
+    // Announce each event
+    confirmedEvents.forEach((e) => {
+      logger.info(`Contract Events :: Processing Event :: '${e.event}'`);
+      contractEventListener.emit(e.event, e.returnValues, e);
+    });
+
+    // Announce that all events were announced
+    contractEventListener.emit('EventsProcessed', proccessData);
+
+    // Avoid polluting production logs
+    const log = confirmedEvents.length > 0 ? logger.info : logger.debug;
+    log(`Contract Events :: Last processed block :: ${toBlock}`);
+
+    return toBlock;
   });
-  if (confirmedEvents.length > 0) {
-    // saveLastProcessedBlock
-    const lastBlock = Math.max(...confirmedEvents.map(e => e.blockNumber));
-    eventBlocks.put('last_processed_block', lastBlock);
-    logger.info(`Contract Events :: Last processed block :: ${lastBlock}`);
-  } else {
-    logger.info('Contract Events :: No events to process');
-  }
-};
 
 Object.assign(contractEventListener, {
   addContract(contract) {
