a6c184b8bd702e6d6d7a37cd15fa5efd17de4ee8
Nahuel Santoalla <nahjersan@gmail.com>
2019-05-21 16:53:49 -0300

    check notaries existance on create data order

--- buyer-api/test/operations/createDataOrder.mock.js
+++ buyer-api/test/operations/createDataOrder.mock.js
@@ -6,8 +6,10 @@ td.replace('uuid/v4', () => 'uuid');
 td.replace('../../src/utils/wibson-lib/coin', { fromWib: () => 'wibTokens' });
 export const getBuyerInfo = sinon.stub();
 td.replace('../../src/services/buyerInfo', { getBuyerInfo });
-export const dataOrders = { store: sinon.spy() };
-td.replace('../../src/utils/stores', { dataOrders });
+export const { dataOrders, notaries } = td.replace('../../src/utils/stores', {
+  dataOrders: { store: sinon.spy() },
+  notaries: { fetch: sinon.spy() },
+});
 export const addTransactionJob = sinon.spy();
 td.replace('../../src/queues/transactionQueue', { addTransactionJob });
 
