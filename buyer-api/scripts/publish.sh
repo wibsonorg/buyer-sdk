ce64cca8dc858527996cc91f535425d9fd4217e9
Nico <ncls.ayala@gmail.com>
2019-05-23 10:56:39 -0300

    Fix publish script

--- buyer-api/scripts/publish.sh
+++ buyer-api/scripts/publish.sh
@@ -7,8 +7,11 @@ mkdir artifacts package
 cp -r contracts package/
 cp -r dist package/
 cp -r config package/
-cp -r node_modules package/
-cp package.json package/
+cp -r scripts package/
 cp .env.example package/
+cp package.json package/
+cd package
+npm i --only=prod
+cd ..
 tar -zcf artifacts/$package_name.tar.gz -C package .
 rm -r package
