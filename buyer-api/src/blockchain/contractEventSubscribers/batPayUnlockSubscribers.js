ec000bc55751bcb765ada8e5d3470d1af3e8a83f
Nico Sommi <nicosommi@gmail.com>
2019-07-01 18:25:52 -0300

    Partial advance on payment unlocked event handler

--- buyer-api/src/blockchain/contractEventSubscribers/batPayUnlockSubscribers.js
+++ buyer-api/src/blockchain/contractEventSubscribers/batPayUnlockSubscribers.js
@@ -1,11 +1,21 @@
+import { fetchTxData } from '../contracts';
+
 /**
+ * @typedef PaymentUnlockedEventValues
+ * @property {string} buyer The buyer that created the DataOrder
  * @callback onPaymentUnlocked Is triggered when the payment to the seller is unlocked
  * @param {UnlockEventValues} eventValues The values emmited by the BatPay PaymentUnlocked event
  */
-export const onPaymentUnlocked = async ({ payIndex, key: masterKey }) => {
+export const onPaymentUnlocked = async ({ payIndex, key: masterKey }, { transactionHash }) => {
   // unlock handler
-  console.log({ payIndex, masterKey });
-  // get seller key from level
+  // get order id and seller id
+  // get seller's key from level (notarizations)
+  const txInput = await fetchTxData({ transactionHash });
+  console.log({ payIndex, masterKey, txInput });
+
+  // decrypt key with masterKey
+  // using decryptionKeyEncryptedWithMasterKey
+  // get data from s3
   // decrypt data
   // append data to s3
   // store email to level?
