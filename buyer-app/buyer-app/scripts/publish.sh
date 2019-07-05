5446938c001f1e7db7b8d5ed7e6db5565c461057
Daniel Fernandez <daniel@grandata.com>
2019-07-05 12:58:01 -0300

    Configs done by buyer

--- buyer-app/buyer-app/scripts/publish.sh
+++ buyer-app/buyer-app/scripts/publish.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 package_name=$1
+package_version=$2
 
 rm -rf artifacts
 mkdir artifacts
-tar -zcf artifacts/${package_name}.tar.gz -C build .
+tar -zcf artifacts/${package_name}-bda-v${package_version}.tar.gz -C build-bda .
+tar -zcf artifacts/${package_name}-jampp-v${package_version}.tar.gz -C build-jampp .
