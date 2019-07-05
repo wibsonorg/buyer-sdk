0bff1017d552302316a97d9870db64055e2bd0cf
Agustin Modugno <agustin.modugno@grandata.com>
2019-07-05 13:29:52 -0300

    Fix order stats (for staging demo)

--- buyer-api/src/blockchain/contractEventSubscribers/batchPaymentsSubscribers.js
+++ buyer-api/src/blockchain/contractEventSubscribers/batchPaymentsSubscribers.js
@@ -6,17 +6,26 @@ import { orderStats } from '../../utils/stores';
 
 const { batPayId } = config;
 
-export const updateBuyerStats = async ({ payIndex, from, totalNumberOfPayees }) => {
+export const updateBuyerStats = async (
+  { payIndex, from, totalNumberOfPayees }, { transactionHash },
+) => {
   if (batPayId !== Number(from)) return; // We didn't perform this payment
   const { metadata: dxHash } = await BatPay.methods.payments(payIndex).call();
-  const { gasPrice } = await web3.eth.getTransaction(dxHash);
-  const { gasUsed, logs } = await web3.eth.getTransactionReceipt(dxHash);
+  const { logs } = await web3.eth.getTransactionReceipt(dxHash);
+  const { gasUsed } = await web3.eth.getTransactionReceipt(transactionHash);
+  const { gasPrice } = await web3.eth.getTransaction(transactionHash);
   const ethUsed = Number(gasPrice) * Number(gasUsed);
   const { orderId } = await decodeLogs(logs);
-  orderStats.update(Number(orderId), [{
-    ethSpent: ethUsed,
-    amountOfPayees: Number(totalNumberOfPayees),
-  }], []);
+
+  await orderStats.update(
+    Number(orderId),
+    oldStat => ({
+      ethSpent: oldStat.ethSpent + ethUsed,
+      amountOfPayees: oldStat.amountOfPayees + Number(totalNumberOfPayees),
+      paymentsRegistered: oldStat.paymentsRegistered + 1,
+    }),
+    { ethSpent: 0, amountOfPayees: 0, paymentsRegistered: 0 },
+  );
 };
 
 export const onPaymentRegistered = jobify(updateBuyerStats);
