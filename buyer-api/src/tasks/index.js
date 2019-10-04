52f27f25e8ea8e1adc715f13ae78c13b01f9751a
Daniel Fernandez <daniel@grandata.com>
2019-10-03 21:03:11 -0300

    Runnable Tasks

--- buyer-api/src/tasks/index.js
+++ buyer-api/src/tasks/index.js
@@ -4,26 +4,54 @@ import '@babel/polyfill';
 import glob from 'glob';
 import loadEnv from '../utils/wibson-lib/loadEnv';
 
+const getTaskName = (path, captureName) => path.replace(captureName, '$1').replace(/\//g, ':');
+
+const getPathTasks = (path, captureName) => {
+  const tasks = require(path).default;
+  if (typeof tasks === 'object') {
+    return tasks;
+  }
+  return {
+    [getTaskName(path, captureName)]: tasks,
+  };
+};
+
+const getTasks = () => {
+  const paths = glob.sync(`${__dirname}/**/*.js`, { ignore: `${__dirname}/index.js` });
+  const captureName = new RegExp(`${__dirname}/(.*).js`);
+
+  return paths.reduce(
+    (accumulator, path) => ({
+      ...accumulator,
+      ...getPathTasks(path, captureName),
+    }),
+    {},
+  );
+};
+
 const init = async () => {
-  await loadEnv();
-  const logger = require('../utils/logger');
-
-  const [taskName, ...args] = process.argv.slice(2);
-  const paths = glob.sync(`${__dirname}/*.js`, { ignore: `${__dirname}/index.js` });
-  const tasks = paths.reduce((accumulator, path) => ({
-    ...accumulator,
-    ...require(path).default,
-  }), {});
-
-  if (!taskName) {
-    logger.info(`Available tasks:\n${Object.keys(tasks).join('\n')}`);
-  } else {
+  let logger;
+  try {
+    await loadEnv();
+    logger = require('../utils/logger');
+    const tasks = getTasks();
+    const [taskName, ...args] = process.argv.slice(2);
     const task = tasks[taskName];
-    if (!task) {
-      logger.info(`Task does not exist '${taskName}'`);
-    } else {
+
+    if (task) {
       logger.info(`Running task ${taskName}`);
       await task(...args);
+      logger.info('Done');
+    } else if (!taskName) {
+      logger.info(`Available tasks:\n${Object.keys(tasks).join('\n')}`);
+    } else {
+      logger.info(`Task '${taskName}' does not exist.`);
+    }
+  } catch (error) {
+    if (logger && logger.error) {
+      logger.error(error);
+    } else {
+      console.error(error); // eslint-disable-line no-console
     }
   }
 
